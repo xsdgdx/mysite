584b4ccab102d2d163e3517f1583014062ace368
xsdgdx <894501246@qq.com>
2018-11-29 17:37:45 +0800

    初始化项目

---
+++ read_statistics/utils.py
@@ -0,0 +1,31 @@
+import datetime
+from django.contrib.contenttypes.models import ContentType
+from .models import ReadNum,ReadDetail
+from django.utils import timezone
+from django.db.models import Sum
+def read_statistics_once_read(request,obj):
+    ct=ContentType.objects.get_for_model(obj)
+    key="%s_%s_read"%(ct.model,obj.pk)
+
+    if not request.COOKIES.get(key):
+        #
+        readnum,created=ReadNum.objects.get_or_create(content_type=ct,object_id=obj.pk)
+        readnum.read_num +=1
+        readnum.save()
+        date=timezone.now().date()
+        readDetail,created=ReadDetail.objects.get_or_create(content_type=ct,object_id=obj.pk,date=date)
+        readDetail.read_num+=1
+        readDetail.save()
+    return key
+
+def get_seven_days_read_date(content_type):
+    today=timezone.now().date()
+    dates=[]
+    read_nums=[]
+    for i in range(7,0,-1):
+        date=today-datetime.timedelta(days=i)
+        dates.append(date.strftime('%m/%d'))
+        read_details=ReadDetail.objects.filter(content_type=content_type,date=date)
+        result=read_details.aggregate(read_num_sum=Sum('read_num'))
+        read_nums.append(result['read_num_sum'] or 0)
+    return dates,read_nums
