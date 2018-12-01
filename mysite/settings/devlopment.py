288d78c20cf9ae549e9c87a5d6c18363f8fad411
xsdgdx <894501246@qq.com>
2018-12-01 21:38:04 +0800

    网站第三次小改动

--- mysite/settings/devlopment.py
+++ mysite/settings/devlopment.py
@@ -20,12 +20,26 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+DATABASE_PASSWORD='123456'
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
+        'NAME': 'mysite_db',         # 你要存储数据的库名，事先要创建之
+        'USER': 'root',         # 数据库用户名
+        'PASSWORD': DATABASE_PASSWORD,     # 密码
+        'HOST': 'localhost',    # 主机
+        'PORT': '3306',         # 数据库使用的端口
+    }
+}
+
+
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}'''
+
 
 
 
