eb4994fb18840cfd124e47d21ac57ab14bbace3e
xsdgdx <894501246@qq.com>
2018-12-04 20:11:56 +0800

    刚才的小改动，疏忽了

--- mysite/settings/development.py
+++ mysite/settings/development.py
@@ -37,21 +37,3 @@ DATABASES = {
     }
 }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'E:/mysite_debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-},
-}
