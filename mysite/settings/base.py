1482500d4af98e6a3043a610a1559eff19fedc97
xsdgdx <894501246@qq.com>
2018-12-04 20:05:21 +0800

    编辑器功能添加

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -115,20 +115,20 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CKEDITOR_UPLOAD_PATH = 'upload/'
 
 CKEDITOR_CONFIGS = {
-    'comment_ckeditor': {
+    'my_ckeditor': {
         'skin': 'moono-lisa',
         'toolbar_Basic': [
             ['Source', '-', 'Bold', 'Italic']
         ],
         'toolbar_Full': [
-            ['Styles', 'Format', 'Bold', 'Italic', 'Underline', 'Strike', 'SpellChecker', 'Undo', 'Redo'],
+            ['Styles', 'Format','Font','FontSize', 'Bold', 'Italic', 'Underline', 'Strike', 'SpellChecker', 'Undo', 'Redo'],
             ['Link', 'Unlink', 'Anchor'],
             ['Image', 'Flash', 'Table', 'HorizontalRule'],
-            ['TextColor', 'BGColor'],
+            ['TextColor', 'BGColor','RemoveFormat'],
+            ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
             ['Smiley', 'SpecialChar'],
             # 在工具栏中添加该功能的按钮
             ['CodeSnippet'], ['Source'],
-
         ],
         'height': 291,
         'width': 'auto',
@@ -136,6 +136,21 @@ CKEDITOR_CONFIGS = {
         'removePlugins': 'elementspath',
         'resize_enabled': False,
         'extraPlugins': 'codesnippet',
+    },
+    'comment_ckeditor': {
+        'toolbar': 'custom',
+        'toolbar_custom': [
+            ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript'],
+            ["TextColor", "BGColor", 'RemoveFormat'],
+            ['NumberedList', 'BulletedList'],
+            ['Link', 'Unlink'],
+            ["Smiley", "SpecialChar", 'Blockquote'],
+        ],
+        'width': 'auto',
+        'height': '180',
+        'tabSpaces': 4,
+        'removePlugins': 'elementspath',
+        'resize_enabled': False,
     }
 }
 
--- mysite/settings/development.py
+++ mysite/settings/development.py
@@ -37,4 +37,21 @@ DATABASES = {
     }
 }
 
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'E:/mysite_debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+},
+}
